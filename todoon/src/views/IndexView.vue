c320b7b26adcacbe01d34ab01ec27c02ac0f5b92
Adrienkgz <127536195+Adrienkgz@users.noreply.github.com>
2024-03-05 20:30:28 +0000

    Changement on the naming

---
+++ todoon/src/views/IndexView.vue
@@ -0,0 +1,25 @@
+<template>
+  <HeaderComponent/>
+  <InfoAccueil/>
+  <StatsAcceuil/>
+  <LoginPage/>
+  <noticeAccueil/>
+  <MainFooter/>
+</template>
+<script>
+import InfoAccueil from '../components/InfoAccueil.vue'
+import StatsAcceuil from '../components/StatsAcceuil.vue'
+import noticeAccueil from '../components/NoticeAccueil.vue'
+import MainFooter from '../components/MainFooter.vue'
+import HeaderComponent from '../components/HeaderComponent.vue'
+
+export default {
+  components: {
+    HeaderComponent,
+    InfoAccueil,
+    StatsAcceuil,
+    noticeAccueil,
+    MainFooter
+  }
+}
+</script>
