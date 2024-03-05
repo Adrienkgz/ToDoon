1973af25f9b6a0b530328608281b911f87872850
dptmathis <mathisdudupont@gmail.com>
2024-03-05 15:24:45 -0500

    carousel

--- todoon/src/components/NoticeAccueil.vue
+++ todoon/src/components/NoticeAccueil.vue
@@ -1,22 +1,18 @@
 <template>
     <smallLine/>
     <div class="flex relative space-x-8 px-24">
-      <noticeCards :userName="'Louis'" :userImage="'3000 users'"/>
-      <noticeCards :userName="'Louis'" :userImage="'3000 users'"/>
-      <noticeCards :userName="'Louis'" :userImage="'3000 users'"/>
-      <noticeCards :userName="'Louis'" :userImage="'3000 users'"/>
-      <noticeCards :userName="'Louis'" :userImage="'3000 users'"/>
+        <carousel/>
     </div>
     <smallLine/>
 </template>
 <script>
-import noticeCards from './cards/noticeCards.vue'
 import smallLine from './line/smallLine.vue'
+import carousel from './carousel/carouselAccueil.vue'
 
 export default {
   components: {
-    noticeCards,
-    smallLine
+    smallLine,
+    carousel
   }
 }
 </script>
---
+++ todoon/src/components/carousel/carouselAccueil.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="carousel carousel-center rounded-box">
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+    <div class="carousel-item">
+        <Card/>
+    </div>
+</div>
+</template>
+<script>
+import Card from '../cards/noticeCards.vue'
+export default {
+  components: {
+    Card
+  }
+}
+</script>
