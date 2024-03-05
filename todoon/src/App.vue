d6d887caacf7c9c782b9fec038f418d67b109c29
dptmathis <mathisdudupont@gmail.com>
2024-03-04 22:13:04 -0500

    button

--- todoon/src/App.vue
+++ todoon/src/App.vue
@@ -1,14 +1,17 @@
 
 <template>
   <HeaderComponent/>
+  <InfoAccueil/>
 </template>
 
 <script>
 import HeaderComponent from './components/header.vue'
+import InfoAccueil from './components/InfoAccueil.vue'
 
 export default {
   components: {
-    HeaderComponent
+    HeaderComponent,
+    InfoAccueil
   }
 }
 </script>
--- todoon/src/components/InfoAccueil.vue
+++ todoon/src/components/InfoAccueil.vue
@@ -1,14 +1,45 @@
 <template>
-    <section class="sm:mx-10 md:mx-20 lg:mx-50">
+    <div class=" sm:mx-10 md:mx-20 lg:mx-50">
         <div class="flex justify-center space-x-5 items-center">
             <div class="container mx-10">
                 <p class="sm:text-lg md:text-6xl font-black">ToDOON</p>
                 <p class="sm:text-lg md:text-6xl font-black py-4">Ultimate task</p>
                 <p class="sm:text-lg md:text-6xl font-black">Management tool</p>
                 <p class="mt-2 sm:text-sm md:text-2xl pr-20">TaskMaster is a task management website — the best way to organize, prioritize, and track your tasks efficiently.</p>
-                <button class="mt-6 bg-pink-600 text-black text-2xl px-5 py-2 rounded-lg w-60 h-20 hover:text-white hover:bg-pink-500">Start Using</button>
+                <swipe-button :infoButton="'Start Using'" class="mt-3"/>
             </div>
             <img class="w-1/3 h-1/3 rounded-lg" src="../assets/img/firstImg.jpeg" alt="Task">
         </div>
-    </section>
+        <div class="flex justify-center">
+            <button @click="scrollToTop" class="image-button">
+                <img src="../assets/img/chevron-down.png" alt="Button Image" class="button-image">
+            </button>
+        </div>
+    </div>
 </template>
+
+<script>
+import SwipeButton from './button/SwipeButton.vue'
+
+export default {
+  components: {
+    SwipeButton
+  },
+  methods: {
+    scrollToTop () {
+      console.log('scrollToTop')
+      const button = document.querySelector('.scroll-button')
+      if (button) {
+        const buttonRect = button.getBoundingClientRect()
+        // Calcul de la position de défilement
+        const scrollPosition = buttonRect.top + window.scrollY - buttonRect.height
+        // Vous pouvez également ajouter des options d'animation ici si vous le souhaitez
+        window.scrollTo({
+          top: scrollPosition,
+          behavior: 'smooth'
+        })
+      }
+    }
+  }
+}
+</script>
