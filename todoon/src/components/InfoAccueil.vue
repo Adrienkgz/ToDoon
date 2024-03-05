419258c544249a75585b4bbb5a61f782bbe8b3d7
dptmathis <mathisdudupont@gmail.com>
2024-03-04 20:06:39 -0500

    infoAccueil

---
+++ todoon/src/components/InfoAccueil.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="sm:mx-10 md:mx-20 lg:mx-50">
+        <div class="flex justify-center space-x-5 items-center">
+            <div class="container mx-10">
+                <p class="sm:text-lg md:text-6xl font-black">ToDOON</p>
+                <p class="sm:text-lg md:text-6xl font-black py-4">Ultimate task</p>
+                <p class="sm:text-lg md:text-6xl font-black">Management tool</p>
+                <p class="mt-2 sm:text-sm md:text-2xl pr-20">TaskMaster is a task management website — the best way to organize, prioritize, and track your tasks efficiently.</p>
+                <button class="mt-6 bg-pink-600 text-black px-5 py-2 rounded-lg w-39 h-12">Start Using</button>
+            </div>
+            <img class="w-1/3 h-1/3 rounded-lg" src="../assets/img/firstImg.jpeg" alt="Task">
+        </div>
+    </section>
+</template>
