e3620ab17d88f15ff131faec53434f3cd021ad1e
Adrienkgz <127536195+Adrienkgz@users.noreply.github.com>
2024-03-05 18:59:01 +0000

    new file:   todoon/src/components/AddTaskComponent.vue 	modified:   todoon/src/components/HeaderHomeComponent.vue 	new file:   todoon/src/components/NotificationIconComponent.vue 	new file:   todoon/src/components/ProfilIconComponent.vue

---
+++ todoon/src/components/NotificationIconComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <a>
+        <img src="../assets/img/logo-notifications.png" alt="notification" class="w-10 h-10">
+    </a>
+</template>
+
+<style>
+a {
+    cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+  name: 'NotificationIconComponent',
+  props: {
+  }
+}
+</script>
---
+++ todoon/src/components/ProfilIconComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <a>
+        <img src="../assets/img/userImage.jpeg" alt="profil" class="w-10 h-10 rounded-4xl">
+    </a>
+</template>
+
+<style>
+a {
+    cursor: pointer;
+}
+<script>
+export default {
+  name: 'ProfilIconComponent',
+  props: {
+  }
+}
+</script>
