2e0bf3666dd2e3b116dd0a0959e9303cd7921e2a
dptmathis <mathisdudupont@gmail.com>
2024-03-05 09:18:49 -0500

    gngng

--- todoon/src/App.vue
+++ todoon/src/App.vue
@@ -1,24 +1,10 @@
 <template>
   <HeaderComponent/>
-  <InfoAccueil/>
-  <StatsAcceuil/>
-  <noticeCards/>
-  <div class="h-96">coucou</div>
   <RouterView/>
 </template>
 
 <script>
 import HeaderComponent from './components/header.vue'
-import InfoAccueil from './components/InfoAccueil.vue'
-import StatsAcceuil from './components/StatsAcceuil.vue'
-import noticeCards from './components/cards/noticeCards.vue'
-
-export default {
-  components: {
-    HeaderComponent,
-    InfoAccueil,
-    StatsAcceuil,
-    noticeCards
 
 export default {
   components: {
