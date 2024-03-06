5eddd118810bac5f3419b890aa155596de7d149e
dptmathis <mathisdudupont@gmail.com>
2024-03-06 17:13:08 -0500

    search bar

--- todoon/src/components/search/SearchBarComponent.vue
+++ todoon/src/components/search/SearchBarComponent.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="search-wrapper" :class="{ active: isActive }">
-    <div class="input-holder">
-      <input type="text" class="search-input" placeholder="Type to search" v-model="searchText" />
-      <button class="search-icon" @click="toggleSearch"><span></span></button>
+  <div class='max-w-md mx-auto'>
+    <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+        <div class="grid place-items-center h-full w-12 text-gray-300">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+        </div>
+        <input class="peer h-full w-full outline-none text-sm text-gray-700 pr-2  border-transparent" type="text" id="search" placeholder="Search something.." />
     </div>
-    <span class="close" @click="toggleSearch"></span>
   </div>
 </template>
 
