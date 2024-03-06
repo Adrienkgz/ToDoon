6272009dff7d802c3e96677893413d7c7498d935
Adrienkgz <127536195+Adrienkgz@users.noreply.github.com>
2024-03-06 19:42:11 +0000

    Ajout du Carousel magique

--- todoon/src/views/HomeView.vue
+++ todoon/src/views/HomeView.vue
@@ -5,6 +5,7 @@
   </aside>
     <main class="w-4/5">
       <HeaderHomeComponent/>
+      <Carousel2Component/>
     </main>
   </div>
 </template>
@@ -17,11 +18,13 @@
 <script>
 import MenuHomeComponent from '../components/homecomponent/MenuHomeComponent.vue'
 import HeaderHomeComponent from '../components/homecomponent/HeaderHomeComponent.vue'
+import Carousel2Component from '../components/carousel/Carousel2.vue'
 
 export default {
   components: {
     MenuHomeComponent,
-    HeaderHomeComponent
+    HeaderHomeComponent,
+    Carousel2Component
   }
 }
 </script>
