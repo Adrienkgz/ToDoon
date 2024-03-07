c674ec005f95c2a51bb2bb807550626d56f1a7d4
thomas973 <160951075+thomas973@users.noreply.github.com>
2024-03-07 18:52:10 +0000

    uhu

--- todoon/src/views/HomeView.vue
+++ todoon/src/views/HomeView.vue
@@ -5,7 +5,6 @@
   </aside>
     <main class="w-4/5">
       <HeaderHomeComponent/>
-      <Carousel1Component/>
       <Carousel2Component/>
     </main>
   </div>
@@ -19,14 +18,12 @@ template {
 <script>
 import MenuHomeComponent from '../components/homecomponent/MenuHomeComponent.vue'
 import HeaderHomeComponent from '../components/homecomponent/HeaderHomeComponent.vue'
-import Carousel1Component from '../components/carousel/Carousel1.vue'
 import Carousel2Component from '../components/carousel/Carousel2.vue'
 
 export default {
   components: {
     MenuHomeComponent,
     HeaderHomeComponent,
-    Carousel1Component,
     Carousel2Component
   }
 }
