8ca1923680f74432296e0a015fb6698f13eac39e
Adrien <adrien.kegreisz@icloud.com>
2024-03-08 15:44:24 -0500

    ajout router link

--- todoon/src/components/homecomponent/MenuHomeComponent.vue
+++ todoon/src/components/homecomponent/MenuHomeComponent.vue
@@ -9,10 +9,10 @@
     <div class="flex-col justify-center text-center mt-5 space-y-10">
         <ul class="m-auto menu menu-vertical bg-base-200 w-56 rounded-box text-xl">
             <li>
-                <a>
+                <router-link to="/home">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="red"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-                High Priority
-                </a>
+                Home
+                </router-link>
             </li>
             <li>
                 <a>
@@ -65,7 +65,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'MenuHomeComponent',
   props: {
