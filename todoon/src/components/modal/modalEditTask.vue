9e708a128fc57a1ee3ac780aca7741105ae61bf9
dptmathis <mathisdudupont@gmail.com>
2024-03-09 16:50:02 -0500

    changement

--- todoon/src/components/modal/modalEditTask.vue
+++ todoon/src/components/modal/modalEditTask.vue
@@ -1,29 +1,14 @@
 <template>
-    <dialog id="modalEdit" class="modal modal-bottom sm:modal-middle">
-    <div class="modal-box">
-        <h3 class="font-bold text-lg">Hello!</h3>
-        <p class="py-4">Press ESC key or click the button below to close</p>
-        <div class="modal-action">
-        <form method="dialog">
-            <!-- if there is a button in form, it will close the modal -->
-            <button class="btn">Close</button>
-        </form>
+    <dialog id="modal_Edit" class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Hello!</h3>
+            <p class="py-4">Press ESC key or click the button below to close</p>
+            <div class="modal-action">
+            <form method="dialog">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn">Close</button>
+            </form>
+            </div>
         </div>
-    </div>
     </dialog>
 </template>
-<script>
-export default {
-  name: 'modalEditButton',
-  props: {
-    task: {
-      type: Object,
-      required: true
-    }
-  },
-  methods: {
-    mounted () {
-        console.log('mounted')
-    }
-  }
-}</script>
