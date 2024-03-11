c852ab0ccff179799352e356fed615c5b49e34aa
dptmathis <mathisdudupont@gmail.com>
2024-03-10 20:47:16 -0400

    jsp

--- server/app/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const user = require('../controllers/user.controller.js')
-    const router = require('express').Router()
-    router.post('/signup', user.signup)
-    router.post('/login', user.login)
-    router.get('/', user.findAll)
-    app.use('/api/user', router)
-  }
--- todoon/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/css/tailwind.css'
-import { register } from 'swiper/element/bundle'
-register()
-
-createApp(App).use(router).mount('#app')
