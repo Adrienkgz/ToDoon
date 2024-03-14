6194157636d3f96af90bed944f01b6ffcdc6f396
dptmathis <mathisdudupont@gmail.com>
2024-03-14 16:21:13 -0400

    login

--- server/app/routes/users.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-  const user = require('../controllers/users.controller.js')
-  const router = require('express').Router()
-    router.post('/signup', user.signup)
-    router.post('/login', user.login)
-    router.get('/', user.findAll)
-    app.use('/api/user', router)
-  }
